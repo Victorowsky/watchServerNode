1d9a3b49bba6d7438b5a476eab17b2afe4921eac
Victorowsky <Victorowskyy@gmail.com>
2021-02-22 21:47:52 +0100

    Home page

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb54efbbd4503733c19bfb4973db5284.js"
+  "/precache-manifest.adf660718f073af2f585114c2867e878.js"
 );
 
 self.addEventListener('message', (event) => {
