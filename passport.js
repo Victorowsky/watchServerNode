0b97d086dcad5e51f03548f56db5559cca404145
Victorowsky <Victorowskyy@gmail.com>
2021-04-22 17:06:13 +0200

    passport twitch callback video.legga.pl

--- passport.js
+++ passport.js
@@ -4,26 +4,26 @@ const twitchStrategy = require("passport-twitch-strategy").Strategy;
 
 const UserSchema = require("./Schemas/UserSchema");
 
-const websiteURL = "https://boiling-bastion-80662.herokuapp.com";
+const websiteURL = "https://video.legga.pl";
 // const websiteURL = "http://localhost:3001";
 
 passport.use(
-  new twitchStrategy(
-    {
-      clientID: process.env.TWITCH_CLIENT_ID,
-      clientSecret: process.env.TWITCH_CLIENT_SECRET,
-      callbackURL: `${websiteURL}/auth/twitch/callback`,
-      scope: "user_read",
-    },
-    function (accessToken, refreshToken, profile, done) {
-      // Suppose we are using mongo..
-      const { id, profile_image_url, displayName } = profile;
-      UserSchema.findOrCreate(
-        { twitchId: id, login: displayName, image: profile_image_url },
-        function (err, user) {
-          return done(err, user);
-        }
-      );
-    }
-  )
+	new twitchStrategy(
+		{
+			clientID: process.env.TWITCH_CLIENT_ID,
+			clientSecret: process.env.TWITCH_CLIENT_SECRET,
+			callbackURL: `${websiteURL}/auth/twitch/callback`,
+			scope: "user_read",
+		},
+		function (accessToken, refreshToken, profile, done) {
+			// Suppose we are using mongo..
+			const { id, profile_image_url, displayName } = profile;
+			UserSchema.findOrCreate(
+				{ twitchId: id, login: displayName, image: profile_image_url },
+				function (err, user) {
+					return done(err, user);
+				}
+			);
+		}
+	)
 );
