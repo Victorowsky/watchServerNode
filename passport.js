021d54f5ab03723027d9907c3395c1ea46d3d9d4
Victorowsky <Victorowskyy@gmail.com>
2021-05-01 18:31:00 +0200

    change to CONST callback uri

--- passport.js
+++ passport.js
@@ -9,7 +9,7 @@ passport.use(
 		{
 			clientID: process.env.TWITCH_CLIENT_ID,
 			clientSecret: process.env.TWITCH_CLIENT_SECRET,
-			callbackURL: `/auth/twitch/callback`,
+			callbackURL: `https://legga.pl/auth/twitch/callback`,
 			scope: "user_read",
 		},
 		function (accessToken, refreshToken, profile, done) {
